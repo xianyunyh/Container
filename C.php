c49ec5becd129842ca5325f070ec2d45d80c89cb
xianyun <xianyun1991@gamil.com>
2017-06-26 00:00:21 +0800

    first init container

---
+++ C.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tianlei
+ * Date: 17-6-25
+ * Time: 下午9:48
+ */
+
+namespace Tests;
+
+
+class C
+{
+    function __construct(D $d)
+    {
+    }
+
+}
---
+++ D.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tianlei
+ * Date: 17-6-25
+ * Time: 下午9:48
+ */
+
+namespace Tests;
+
+
+class D
+{
+    public function __construct(C $c)
+    {
+    }
+
+    public function test()
+    {
+        return "this test in D";
+    }
+
+}
